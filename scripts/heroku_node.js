75042f643f7a145528c9f4a687983e418b66a0ba
camzillajs101 <camilomason0@gmail.com>
2017-05-31 16:09:59 -0400

    Tweaking heroku_node.js to fix

--- scripts/heroku_node.js
+++ scripts/heroku_node.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req,res){
   res.send('Hello World!');
