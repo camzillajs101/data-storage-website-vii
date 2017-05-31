43af2efca618483f932cb0cca671d892f290f390
camzillajs101 <camilomason0@gmail.com>
2017-05-31 16:10:23 -0400

    Adding resetPassword function, cleaning code

--- scripts/usermain.js
+++ scripts/usermain.js
@@ -1,8 +1,9 @@
-CB.CloudApp.init(env.CB_app_id,env.CB_client_key);
+CB.CloudApp.init(env.CB_APPID,env.CB_CLIENTKEY);
 let user = new CB.CloudUser();
 function signUp(form){
-  $('.form-error-text').html();
-  if ($(form.username).val() === "" || $(form.password).val() === "" || $(form.confirm_pass).val() === ""){
+  $('.form-error-text').html("");
+  $('input').css("border","1px solid gray");
+  if ($(form.username).val() === "" || $(form.password).val() === "" || $(form.confirm_pass).val() === "" || $(form.email).val() === ""){
     if ($(form.username).val() === ""){
       $('input[name="username"]').css("border","1.5px solid red");
     };
@@ -12,19 +13,18 @@ function signUp(form){
     if ($(form.confirm_pass).val() === ""){
       $('input[name="confirm_pass"]').css("border","1.5px solid red");
     };
-    $('.form-error-text').html("You must fill in all inputs.")
+    if ($(form.email).val() === ""){
+      $('input[name="email"]').css("border","1.5px solid red");
+    }
+    $('.form-error-text').html("You must fill in all inputs.");
   } else {
     if (form.password.value === form.confirm_pass.value){
       user.set('username',form.username.value);
       user.set('password',form.password.value);
-      if (form.email.value === ""){
-        user.set('email',"undefined@zero.null");
-      } else {
-        user.set('email',form.email.value);
-      };
+      user.set('email',form.email.value);
       user.signUp({
         success: function(new_user){
-          window.location.replace("main-nav-page.html");
+          window.location.replace("views/main.html");
         },
         error: function(error){
           $('.form-error-text').html(error.response.data.error);
@@ -36,7 +36,8 @@ function signUp(form){
   };
 };
 function logIn(form){
-  $('.form-error-text').html();
+  $('.form-error-text').html("");
+  $('input').css("border","1px solid gray");
   if ($(form.username).val() === "" || $(form.password).val() === ""){
     if ($(form.username).val() === ""){
       $('input[name="username"]').css("border","1.5px solid red");
@@ -46,8 +47,8 @@ function logIn(form){
     };
     $('.form-error-text').html("You must fill in all inputs.");
   } else {
-    user.set('username',form.username.value);
-    user.set('password',form.password.value);
+    user.set('username',$(form.username).val);
+    user.set('password',$(form.password).val);
     user.logIn({
       success: function(new_user){
         window.location.replace("views/main.html");
@@ -65,6 +66,16 @@ function logOut(){
     },
     error: function(error){
       console.log(error.response.data.error);
+    }
+  });
+};
+function resetPassword(form){
+  CB.CloudUser.resetPassword($(form.email).val(),{
+    success: function(){
+      $('.form-error-text').html("Your email has been sent.");
+    },
+    error: function(error){
+      $('.form-error-text').html(error.response.data.error);
     }
   });
 };
