7060316ba580010bae8d87653b649d5d6ef9a522
camzillajs101 <camilomason0@gmail.com>
2017-06-03 10:28:29 -0400

    Adding fake routes, testing res.render, soft coding port

--- scripts/heroku_node.js
+++ scripts/heroku_node.js
@@ -2,11 +2,21 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs');
+app.use('/static',express.static('public'));
+app.use('/view',express.static('static/views'));
+app.use('/image',express.static('images'));
+app.use('/script',express.static('scripts'));
+app.use('/style',express.static('styles'));
+app.use('/font',express.static('fonts'));
+app.use('/root',express.static('/'));
 
 app.get('/', function (req,res){
-  res.send('Hello World!');
-})
+  res.render('static/login.html', function(err, html) {
+    res.send(html);
+  });
+});
 
-app.listen(3000, function(){
-  console.log('C-Mail listening on port 3000!')
-})
+app.listen(app.get('port'), function(){
+  console.log(`C-Mail listening on port ${app.get('port')}!`);
+});
